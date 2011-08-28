76eed97cd9f3b1a745a393d3202e26430d66be4a
bill.zhuang <zhuangmin@gmail.com>
2011-08-28 19:52:36 +0800

    add function for create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,13 +9,31 @@ class CategoriesController < ApplicationController
       end
     end
   end
+
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      respond_to do |format|
+        format.json do
+          render :json => {:result => "success", :category =>@category}
+        end
+      end
+    else
+      respond_to do |format|
+        format.json do
+          render :json => {:result => "fail"}
+        end
+      end
+    end
+  end
+  
   
   def hot
     #@result = Category.find_by_sql("select c.name, count(t.id) as tac from categories as c left join targets as t on c.id = t.category_id group by c.name").limit(10)    
     @result = Category.find(:all, :select => 'categories.name,categories.id, count(targets.id) as tac',:joins => 'left outer join targets on targets.category_id = categories.id', :group => 'categories.id',:order => 'count(targets.id) DESC', :limit => 10)
     respond_to do |format|
       format.json do
-        render :json => {:result => "success", :categoryList => @result.as_json}
+        render :json => {:result => "success", :categoryList => @result}
       end
       format.html do
       end
