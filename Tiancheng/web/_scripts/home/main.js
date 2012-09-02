7b1abcc7ba3c4c95413f4c5be12dd07ca2e4f77c
faceach <faceach@gmail.com>
2012-09-02 21:45:11 +0800

    update kudos

---
+++ Tiancheng/web/_scripts/home/main.js
@@ -0,0 +1,17 @@
+﻿require.config({
+    baseUrl: "./_scripts",
+    paths: {
+        "class": "_lib/class",
+        "block": "_lib/block",
+        "slideshow": "_lib/slideshow"
+    },
+    urlArgs: "version=1.0"
+});
+
+require(["jquery", "slideshow"], function ($, slideshow) {
+    $(document).ready(function () {
+
+        slideshow($("#banner")).initialize();
+
+    });
+});
